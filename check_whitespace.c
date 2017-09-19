83d7ceddd26e7a159c8ecd014a3640b5aa3cf584
Charles Menne <menne019@macross.morris.umn.edu>
2017-09-18 19:31:14 -0500

    Compiled check_whitespace.c and checked fo leaks

--- check_whitespace.c
+++ check_whitespace.c
@@ -46,7 +46,9 @@ char* strip(char* str) {
   // Place the null terminator at the end of the result string.
   result[i-first_non_space] = '\0';
 
+  
   return result;
+  
 }
 
 /*
@@ -66,7 +68,9 @@ int is_clean(char* str) {
   // 0 if they're equal, and a positive value if the first is
   // greater than the second.
   result = strcmp(str, cleaned);
-
+  if (strlen(cleaned) > 0) {
+    free(cleaned);
+      }
   return result == 0;
 }
 
